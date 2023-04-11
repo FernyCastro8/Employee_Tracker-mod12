abc7b4d555a3042cf80c8b8ce402b6722fe417a2
Ferny <fernc28@outlook.com>
2023-04-10 21:47:48 -0400

    initial code

---
+++ sql/query.sql
@@ -0,0 +1,3 @@
+-- A constructor function or class could be helpful for organizing these. 
+--     You might also want to include a seeds.sql file to pre-populate your database, 
+--         making the development of individual features much easier.
