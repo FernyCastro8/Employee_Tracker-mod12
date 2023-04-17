c08f40e0677bd5a528fb64b02965950ac59504ed
Ferny <fernc28@outlook.com>
2023-04-16 20:22:11 -0400

    update employe and quit - options working

--- index.js
+++ index.js
@@ -30,37 +30,15 @@ const viewDepartments = async () => {
 
 // Function async to return view all roles
 const viewRoles = async () => {
-  const results = await db.promise().query("SELECT role.id, role.title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department.id;")
+  const results = await db.promise().query("SELECT role.title, role.id AS role_id, department.name AS department_name, role.salary FROM role JOIN department ON role.department_id = department.id;")
 
   console.table(results[0]);
 };
 
 
-
-//Function to add employee
-// const addEmployee = async () => {
-//   const results = await inquirer.prompt([
-//     {
-//       type: 'input',
-//       name: 'name',
-//       message: 'Enter the name of the department:',
-//       validate: function (value) {
-//         if (value.trim() === '') {
-//           return 'Department name is required';
-//         }
-//         return true;
-//       },
-//     },
-//   ]);
-
-// await db.query('INSERT INTO `employee_db`.`employees` (`first_name`, `last_name`, `role_id`, `manager_id`) VALUES (?, ?, ?, ?)', results)
-// console.table(results[0])
-// };
-
-
 // Function async to add a department
 const addDepartment = async () => {
-  const newDepartment  = await inquirer.prompt({
+  const newDepartment = await inquirer.prompt({
     type: 'input',
     message: 'Enter the name of the department:',
     name: 'departmentName',
@@ -72,7 +50,7 @@ const addDepartment = async () => {
     }
   });
   console.log(newDepartment)
-  
+
   const result = await db.promise().query(
     `INSERT INTO department (name) VALUES (?)`,
     newDepartment.departmentName
@@ -81,14 +59,15 @@ const addDepartment = async () => {
   console.log(`${result[0].affectedRows} department added.`);
 };
 
+
+// Function to add role to department ( role, salary, department )
 const addRole = async () => {
-  const departments = await db.promise().query( 
+  const departments = await db.promise().query(
     'SELECT * FROM department');
-    const departmentChoices = await departments[0].map(({id, name }) => ({
-       name:name,
-       value: id
-    }))
-    // console.log(departmentChoices)
+  const departmentChoices = await departments[0].map(({ id, name }) => ({
+    name: name,
+    value: id
+  }));
 
   const newRole = await inquirer.prompt([{
     type: 'input',
@@ -100,27 +79,148 @@ const addRole = async () => {
     message: 'What is the salary for the role?',
     name: 'salary'
   },
-{
-  type: 'list',
+  {
+    type: 'list',
     message: 'what department does the role belongs to?',
     name: 'department',
     choices: departmentChoices
-}])
+  }])
 
-console.log(newRole);
+  console.log(newRole);
 
-const result = await db.promise().query(
-  `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`,
-  [newRole.role, newRole.salary, newRole.department]
-);
+  const result = await db.promise().query(
+    `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`,
+    [newRole.role, newRole.salary, newRole.department]
+  );
 
-console.log(`${result[0].affectedRows} department added.`);
-}
+  console.log(`${result[0].affectedRows} department added.`);
+};
+
+// Funtion to add employees ( first name, last name, role, and manager )
+const addEmployee = () => {
+  db.query('SELECT * FROM role', (err, results) => {
+
+    const roleChoices = results.map(({ id, title, }) => ({
+      value: id,
+      name: title
+    }));
+
+
+    db.query('SELECT * FROM employee', (err, results) => {
+      const employeeChoices = results.map(({ id, first_name, last_name, }) => ({
+        value: id,
+        name: first_name + ' ' + last_name
+      }));
+
+
+      inquirer.prompt([{
+        type: 'input',
+        message: 'Enter employee first name',
+        name: 'first_name'
+      },
+      {
+        type: 'input',
+        message: 'Enter employee last name',
+        name: 'last_name'
+      },
+      {
+        type: 'list',
+        message: 'what role does the employee belongs to?',
+        name: 'role_id',
+        choices: roleChoices
+      },
+      {
+        type: 'list',
+        message: 'Enter manager id',
+        name: 'manager_id',
+        choices: employeeChoices
+      }
+      ]).then((newEmployee) => {
+
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`,
+          [newEmployee.first_name, newEmployee.last_name, newEmployee.role_id, newEmployee.manager_id]
+          , (err, results) => {
+            console.log(`new employee added.`);
+            menu()
+          });
+      })
+    })
+  });
+};
+
+
+const removeDepartment = () => {
+  db.query('SELECT * FROM department', (err, result) => {
+    const deleteDepartmentChoices = result.map(({ id, name }) => ({
+      value: id,
+      name: name
+    }))
+  });
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Which department would you like to remove',
+        name: 'delete_department',
+        choices: departmentChoices
+      }
+    ]).then((departmentDeleted))
+
+  db.query("DELETE FROM department WHERE name = ?;"), departmentDeleted, (err, results) => {
+    console.log(`department deleteed.`);
+    menu();
+  };
+};
 
 
 
+// Function to updated employee
+const updateEmployee = () => {
+  // In here we are gonna select from the employees table
+  db.query('SELECT * FROM employee', (err, results) => {
+    const employeeChoices = results.map(({ id, first_name, last_name, }) => ({
+      value: id,
+      name: first_name + ' ' + last_name
+      //  + " " + //  used to concat both first_name and last_name
+    }));
 
-// question resquet by bussines owner
+    // In here we are gonna select from the role table
+    db.query('SELECT * FROM role', (err, results) => {
+
+      const roleChoices = results.map(({ id, title, }) => ({
+        value: id,
+        name: title
+      }));
+
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            message: "Select an employee you want to updated",
+            name: 'employee_id ',
+            choices: employeeChoices
+          },
+          {
+            type: 'list',
+            message: "Select employee's new role",
+            name: 'role_id',
+            choices: roleChoices
+          }
+        ])
+        .then((answers) => {
+          db.query('UPDATE employee SET role_id = ? WHERE id = ?', [answers.role_id, answers.employee_id], (err, results) => {
+            console.log(`Employee Updated.`);
+            menu()
+          })
+        })
+    })
+  })
+};
+
+
+
+// Question resquet by bussines owner
 const menu = async () => {
   const results = await inquirer
     .prompt([
@@ -128,22 +228,42 @@ const menu = async () => {
         type: 'list',
         message: 'welcome to employee tracker, what would you like to do?',
         name: 'menu',
-        choices: ['View all employees', 'View all departments', 'View all roles', 'Add employee', 'Add department', 'Add role', 'Remove department', 'Remove role', 'Remove employee']
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add role', 'Add employee', 'Remove department', 'Remove role', 'Remove employee', 'Update Employee', 'Quit']
       }
     ])
-  if (results.menu === 'View all employees') {
-    viewEmployees().then(() => {menu()})
-  } if (results.menu === 'View all departments') {
-    viewDepartments().then(() => {menu()})
-  } if (results.menu === 'View all roles') {
-    viewRoles().then(() => {menu()})
-  } if (results.menu === 'Add department') {
-    addDepartment().then(() => {menu()})
-  } if (results.menu === 'Add role') {
-    addRole().then(() => {menu()})
+
+
+  switch (results.menu) {
+    case 'View all employees':
+      viewEmployees().then(() => { menu() });
+      break;
+    case 'View all departments':
+      viewDepartments().then(() => { menu() });
+      break;
+    case 'View all roles':
+      viewRoles().then(() => { menu() });
+      break;
+    case 'Add department':
+      addDepartment().then(() => { menu() });
+    case 'Add role':
+      addRole().then(() => { menu() });
+      break;
+    case 'Add department':
+      addRole().then(() => { menu() });
+      break;
+    case 'Add employee':
+      addEmployee();
+      break;
+    case 'Remove department':
+      removeDepartment();
+      break;
+    case 'Update Employee':
+      updateEmployee();
+    default:
+      process.exit();
+    // ( process.exit )  is used to quit or exit the function.
   }
 };
-
 
 menu();
 
